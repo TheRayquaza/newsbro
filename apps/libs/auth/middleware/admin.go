681e09d661b6d2e9efb9e983193ae839b7f3854b
Matéo Lelong <mateo@forge.epita.fr>
2025-09-29 15:25:21 +0200

    apps: libs: add redirect on invalid token

--- apps/libs/auth/middleware/admin.go
+++ apps/libs/auth/middleware/admin.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 
 	"github.com/TheRayquaza/newsbro/apps/libs/auth/entities"
@@ -11,6 +12,7 @@ func AdminMiddleware() gin.HandlerFunc {
 	return gin.HandlerFunc(func(c *gin.Context) {
 		var user, exists = c.Get("user")
 		if user == nil || !exists {
+			log.Println("Unauthorized access attempt to admin route")
 			c.JSON(http.StatusUnauthorized, gin.H{
 				"error":      "Unauthorized",
 				"request_id": c.GetString("requestID"),
@@ -20,6 +22,7 @@ func AdminMiddleware() gin.HandlerFunc {
 		}
 		u := user.(*entities.JWTClaims)
 		if u.Role != "admin" {
+			log.Println("Forbidden access attempt to admin route by user: ", u.Username)
 			c.JSON(http.StatusForbidden, gin.H{
 				"error":      "Forbidden: Admins only",
 				"request_id": c.GetString("requestID"),
