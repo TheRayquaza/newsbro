eec62e3eea72da570ded75a68460bc2afba1edc3
Matéo Lelong <mateo@forge.epita.fr>
2025-09-30 13:39:09 +0200

    apps: libs: add kafka lib

---
+++ apps/libs/kafka/command/new_article_command.go
@@ -0,0 +1,14 @@
+package command
+
+import "time"
+
+type NewArticleCommand struct {
+	Title       string    `json:"title"`
+	Link        string    `json:"link"`
+	Description string    `json:"description"`
+	Content     string    `json:"content"`
+	Author      string    `json:"author"`
+	Category	string    `json:"category"`
+	Subcategory	string    `json:"subcategory"`
+	PublishedAt time.Time `json:"published_at"`
+}
