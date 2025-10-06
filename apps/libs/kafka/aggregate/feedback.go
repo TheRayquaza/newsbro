ed9e3a9e70fe7873c9c7c07228175b852c9332ba
Matéo Lelong <mateo@forge.epita.fr>
2025-10-07 00:13:24 +0200

    apps: repo-article: apply lint

--- apps/libs/kafka/aggregate/feedback.go
+++ apps/libs/kafka/aggregate/feedback.go
@@ -3,6 +3,6 @@ package aggregate
 type FeedbackAggregate struct {
 	UserID   uint `json:"user_id"`
 	NewsID   uint `json:"news_id"`
-	Value    int  `json:"value"` // 0 = dislike, 1 = like
+	Value    int  `json:"value"`     // 0 = dislike, 1 = like
 	IsActive bool `json:"is_active"` // remove feedback if false
 }
--- apps/repo-article/src/data/models/feedback.go
+++ apps/repo-article/src/data/models/feedback.go
@@ -5,13 +5,13 @@ import (
 )
 
 type Feedback struct {
-	ID        uint           `gorm:"primarykey" json:"id"`
-	UserID    uint           `gorm:"not null;index:idx_user_article,unique" json:"user_id"`
-	NewsID    uint           `gorm:"not null;index:idx_user_article,unique;index:idx_news_feedback" json:"news_id"`
-	Article   Article        `gorm:"foreignKey:NewsID;references:ID" json:"article"` // <-- link to Article
-	Value     int            `gorm:"not null;check:value IN (0, 1)" json:"value"` // 0 = dislike, 1 = like
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
+	ID        uint      `gorm:"primarykey" json:"id"`
+	UserID    uint      `gorm:"not null;index:idx_user_article,unique" json:"user_id"`
+	NewsID    uint      `gorm:"not null;index:idx_user_article,unique;index:idx_news_feedback" json:"news_id"`
+	Article   Article   `gorm:"foreignKey:NewsID;references:ID" json:"article"` // <-- link to Article
+	Value     int       `gorm:"not null;check:value IN (0, 1)" json:"value"`    // 0 = dislike, 1 = like
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func (Feedback) TableName() string {
