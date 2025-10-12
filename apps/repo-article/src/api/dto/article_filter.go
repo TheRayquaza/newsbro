e79a1aafb130fb4a2239f96f1abc98a13d31ac9e
Matéo Lelong <mateo@forge.epita.fr>
2025-10-12 12:14:50 +0200

    apps: repo-article: apply lint

--- apps/repo-article/src/api/dto/article_filter.go
+++ apps/repo-article/src/api/dto/article_filter.go
@@ -3,11 +3,11 @@ package dto
 import "time"
 
 type ArticleFilters struct {
-	Category    string `form:"category"`
-	Subcategory string `form:"subcategory"`
-	Search      string `form:"search"`
+	Category    string     `form:"category"`
+	Subcategory string     `form:"subcategory"`
+	Search      string     `form:"search"`
 	BeginDate   *time.Time `form:"begin_date" time_format:"2006-01-02" time_utc:"1"`
 	EndDate     *time.Time `form:"end_date" time_format:"2006-01-02" time_utc:"1"`
-	Limit       uint   `form:"limit,default=10"`
-	Offset      uint   `form:"offset,default=0"`
+	Limit       uint       `form:"limit,default=10"`
+	Offset      uint       `form:"offset,default=0"`
 }
