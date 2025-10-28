f73a02c2425d89a7a6fb5bb00f7208d8c71a45b7
Matéo Lelong <mateo@forge.epita.fr>
2025-10-28 14:34:57 +0100

    apps: libs: add inference command

---
+++ apps/libs/kafka/command/inference_command.go
@@ -0,0 +1,14 @@
+package command
+
+import (
+	"github.com/TheRayquaza/newsbro/apps/libs/kafka/aggregate"
+	"time"
+)
+
+type InferenceCommand struct {
+	UserID  uint      `json:"user_id"`
+	Model   string    `json:"model_name"`
+	Score   float64   `json:"score"`
+	Date    time.Time `json:"date"`
+	Article *aggregate.ArticleAggregate
+}
