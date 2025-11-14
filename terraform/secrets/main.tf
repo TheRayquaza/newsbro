a3a604721755de0ddd61d38aa6c480e7a65e451a
Matéo Lelong <mateo@forge.epita.fr>
2025-11-14 12:13:30 +0100

    terraform: vault: remove postgres minio secrets

--- terraform/secrets/main.tf
+++ terraform/secrets/main.tf
@@ -10,6 +10,7 @@ terraform {
 provider "vault" {
   address = var.vault_address
   token   = var.vault_root_token
+  skip_tls_verify= true
 }
 
 # Enable KV v2 secrets engine if not already enabled
@@ -73,17 +74,6 @@ resource "vault_kv_secret_v2" "srvc_scrapping_postgres" {
     password = var.srvc_scrapping_postgres_password
   })
 }
-
-resource "vault_kv_secret_v2" "postgres-minio" {
-  mount = vault_mount.kv.path
-  name  = "postgres/minio"
-
-  data_json = jsonencode({
-    user = var.postgres_minio_username
-    password = var.postgres_minio_password
-  })
-}
-
 
 # ==========================================
 # Cloudflare Secrets
