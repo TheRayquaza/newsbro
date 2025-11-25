cc6f4c49f328d005f403092dff3b7e4fec304272
Matéo Lelong <mateo@forge.epita.fr>
2025-11-25 23:02:17 +0100

    apps: repo-account: add more logs using zap

--- apps/repo-account/src/data/database/db.go
+++ apps/repo-account/src/data/database/db.go
@@ -5,22 +5,27 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	"github.com/TheRayquaza/newsbro/apps/libs/utils"
 )
 
 func Initialize(databaseURL string) (*gorm.DB, error) {
 	db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
 	if err != nil {
+		utils.SugarLog.Error("Failed to connect to database:", err)
 		return nil, err
 	}
 
-	// Auto-migrate models
+	utils.SugarLog.Info("Connected to database successfully")
 	err = db.AutoMigrate(
 		&models.User{},
 		&models.RefreshToken{},
 	)
 	if err != nil {
+		utils.SugarLog.Error("Failed to auto-migrate database:", err)
 		return nil, err
 	}
+	utils.SugarLog.Info("Database auto-migration completed successfully")
 
 	return db, nil
 }
